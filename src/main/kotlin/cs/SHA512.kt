d5ab46a1c107fa736b9bd5116232eb98a9dd926b
Maksym Maliarov <maxim.maliarov@gmail.com>
2018-02-25 01:17:00 +0200

    Backend finish + removal of HEX dependency

--- src/main/kotlin/cs/SHA512.kt
+++ src/main/kotlin/cs/SHA512.kt
@@ -1,12 +1,21 @@
 package cs
 
-import com.sun.org.apache.xerces.internal.impl.dv.util.HexBin
 import java.security.MessageDigest
 
-fun toSHA512(text: String) : String{
+fun toSHA512(text: String): String {
     val encryptor = MessageDigest.getInstance("SHA-512")
     val textBytes = text.toByteArray(Charsets.UTF_8)
     val hashedBytes = encryptor.digest(textBytes)
 
-    return HexBin.encode(hashedBytes)
+    return toHex(hashedBytes)
+}
+
+fun toHex(bytes: ByteArray): String {
+    val sb = StringBuilder()
+
+    for (index in 0 until bytes.size) {
+        sb.append(String.format("%02X", bytes[index]))
+    }
+
+    return sb.toString()
 }
